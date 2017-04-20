8f8ab3528ddc0352649ccaf9d58d412991f3ea80
Max Gruzin <maxgruzin@gmail.com>
2017-04-20 22:57:52 +0300

    First public version

--- silentflow/apps/sf/tests.py
+++ silentflow/apps/sf/tests.py
@@ -1,10 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Tag, Artist, Release, ReleaseTags, ReleaseArtists, Track
+
+# c = Client()
+# response = c.post('/login/', {'username': 'john', 'password': 'smith'})
+# response.status_code
+# response = c.get('/customer/details/')
+# response.content
 
 
-class TestTests(TestCase):
+# class TagModelTests(TestCase):
+#     def setUp(self):
+#         Tag.objects.create(name="testtag")
+#
+#     def test_tag_creation(self):
+#         test_tag = Tag.objects.get(name="testtag")
+#         self.assertTrue(isinstance(test_tag, Tag))
+#         self.assertEqual(test_tag.__unicode__(), test_tag.title)
 
-    def test_test(self):
-        self.assertEqual('foo', 'foo')
 
-    def test_test2(self):
-        self.assertAlmostEqual('foo', 'foo')
+class IndexViewTests(TestCase):
+
+    def test_index_loads(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertGreaterEqual(len(response.context['releases']), 2)
+        self.assertEqual(response.context['releases'], 2)
+        self.assertEqual(response.context['releases'][0], None)
+        # self.assertGreater
--- silentflow/apps/sf/views.py
+++ silentflow/apps/sf/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Release
 # from django.http import HttpResponse
-
-# Create your views here.
 
 
 def index(request):
-    context = None
-    return render(request, 'index.html', context)
+
+    if request.method == 'GET':
+        releases = Release.objects.filter(is_active=True).order_by('-released_at')[:200]
+
+        return render(request, 'index.html', {'releases': releases})
