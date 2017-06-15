c675da7ef33f02d216b2729cb6b94ca0422d638f
Max Gruzin <maxgruzin@gmail.com>
2017-06-15 22:35:52 +0300

    parallax release images, headlines in release containers

--- silentflow/apps/sf/views.py
+++ silentflow/apps/sf/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from .models import Release
+from .models import Release, Track
 # from django.http import HttpResponse
 
 
 def index(request):
 
     if request.method == 'GET':
-        releases = Release.objects.filter(is_active=True).order_by('-released_at')[:200]
+        releases = Release.objects.filter(is_active=True).order_by('-released_at').values()[:20]
+        # for release in releases:
+        #     tracklist_values = list(Track.objects.filter(release_id=release['id']).order_by('pos').values(
+        #         'pos', 'title', 'slug', 'duration'))
+        #     release['tracks'] = tracklist_values
 
         return render(request, 'index.html', {'releases': releases})
