c99b02e6599376ecc787be85cc474a7bc7882d0a
Max <maxgruzin@gmail.com>
2017-09-27 10:19:57 +0300

    play track fixed

--- static/js/sf/release.js
+++ static/js/sf/release.js
@@ -15,11 +15,12 @@ window.onload = function(){
 
 
     $('#cover-container').on('click', '.btn-play', function(){
+        console.log("button play clicked");
         var id = this.id;
-//        $(this).closest("audio").prop("play");
-        audio = "#pause-" + this.id.split('-')[2];
-        $("btn-pause-993").css( "display", "inline !important" );
-        $(audio).prop("play");
+        $(this).closest("li").find("audio").get(0).play();
+//        audio = "#pause-" + this.id.split('-')[2];
+//        $("btn-pause-993").css( "display", "inline !important" );
+//        $(audio).prop("play");
     });
 
 
