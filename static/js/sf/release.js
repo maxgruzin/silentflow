6170ba9c1f87e2d432087a900ec217b38fbc0629
Max Gruzin <maxgruzin@gmail.com>
2017-10-07 20:49:24 +0300

    navigation header removed,floating button instead, new banner

--- static/js/sf/release.js
+++ static/js/sf/release.js
@@ -15,12 +15,24 @@ window.onload = function(){
 
 
     $('#cover-container').on('click', '.btn-play', function(){
-        console.log("button play clicked");
         var id = this.id;
         $(this).closest("li").find("audio").get(0).play();
-//        audio = "#pause-" + this.id.split('-')[2];
-//        $("btn-pause-993").css( "display", "inline !important" );
-//        $(audio).prop("play");
+        $(this).addClass("hide");
+        $(this).closest("li").find(".btn-pause").removeClass("hide");
+    });
+
+    $('#cover-container').on('click', '.btn-pause', function(){
+        //var id = this.id;
+        $(this).closest("li").find("audio").get(0).pause();
+        $(this).addClass("hide");
+        $(this).closest("li").find(".btn-play").removeClass("hide");
+    });
+
+    $('audio').on('ended', function(){
+        $(this).closest("li").find("audio").get(0).pause();
+        // $(this).addClass("hide");
+        $(this).closest("li").find(".btn-pause").addClass("hide");
+        $(this).closest("li").find(".btn-play").removeClass("hide");
     });
 
 
