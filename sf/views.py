a921d5260013a084fd4b01c07578594f70ab437c
maxgruzin@gmail.com <max010388>
2018-05-12 18:35:47 +0300

    Project structure fix

---
+++ sf/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from .models import *
+from django.http import HttpResponseNotFound
+
+
+def index(request):
+
+    if request.method == 'GET':
+        releases = Release.objects.filter(is_active=True).order_by('-released_at').values()
+        # for release in releases:
+        #     tracklist_values = list(Track.objects.filter(release_id=release['id']).order_by('pos').values(
+        #         'pos', 'title', 'slug', 'duration'))
+        #     release['tracks'] = tracklist_values
+
+        return render(request, 'index.html', {'releases': releases})
+
+
+def artists(request):
+
+    if request.method == 'GET':
+        artists = Artist.objects.filter(is_active=True).order_by('name').values().exclude(id__in=[0, 1]).values()
+        for artist in artists:
+            artists_release_ids = ReleaseArtists.objects.filter(artist=artist['id']).values_list('release__id', flat=True)
+            releases = list(Release.objects.filter(id__in=artists_release_ids, is_active=True).order_by('-id').values(
+                  'catalogue_number', 'name', 'slug', 'cover_image'))
+            artist['releases'] = releases
+
+        return render(request, 'artists.html', {'artists': artists})
+
+
+def catalogue(request):
+
+    if request.method == 'GET':
+        releases = Release.objects.filter(is_active=True).order_by('-released_at').values('catalogue_number', 'name', 'slug')
+
+        return render(request, 'catalogue.html', {'releases': releases})
+
+
+def about(request):
+
+    return render(request, 'about.html')
+
+
+def release(request, slug):
+
+    if request.method == 'GET':
+        release_qset = Release.objects.filter(slug=slug, is_active=True).first()
+
+        if release_qset is None:
+            return HttpResponseNotFound('<h1>Page not found</h1>')
+
+        tags = ReleaseTags.objects.filter(release=release_qset).values_list('tag__name', flat=True)
+
+        tracklist = list(Track.objects.filter(release_id=release_qset).order_by('pos').values(
+            'id', 'pos', 'title', 'slug', 'duration', 'track_mp3'))
+
+        return render(request, 'release.html', {'release': release_qset,
+                                                'tracklist': tracklist,
+                                                'tags': tags})
+
+
+def contact(request):
+
+    return render(request, 'contact.html')
