1509752436bed974ae36b379ba4a89bd0b966557
maxgruzin@gmail.com <max010388>
2018-09-19 22:00:52 +0300

    parallax on index

--- sf/views.py
+++ sf/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponseNotFound
 def index(request):
 
     if request.method == 'GET':
-        releases = Release.objects.filter(is_active=True).order_by('-released_at').values()
+        releases = Release.objects.filter(is_active=True).order_by('-released_at').values()[0:15]
         # for release in releases:
         #     tracklist_values = list(Track.objects.filter(release_id=release['id']).order_by('pos').values(
         #         'pos', 'title', 'slug', 'duration'))
