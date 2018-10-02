6f23b50349942ea668e1700ed8a34983fdf0756e
Alexander <shurlik9999@mail.ru>
2018-10-02 12:09:48 +0300

    Alex Tarakanov upgrade to materialize 1.0.0 beta

--- static/js/sf/base.js
+++ static/js/sf/base.js
@@ -1,3 +1,10 @@
-$( document ).ready(function() {
-    $(".button-collapse").sideNav();
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+        direction: 'right',
+        hoverEnabled: false
+    });
+
 });
