477b4151fbfbcc7baa2f1279fb00bc1e8f2c51d8
Alexander <shurlik9999@mail.ru>
2018-10-02 13:59:50 +0300

    Alex Tarakanov add share buttons on release page

--- static/js/sf/base.js
+++ static/js/sf/base.js
@@ -1,7 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    var elems = document.querySelectorAll('.fixed-action-btn');
+    var elems = document.querySelectorAll('.menu_button.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, {
         direction: 'right',
         hoverEnabled: false
