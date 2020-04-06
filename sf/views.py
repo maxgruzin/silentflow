451f5d5f13a84a1a04c5774623466d6f8936a928
Max <maxgruzin@gmail.com>
2020-04-06 23:51:26 +0300

    release page: artist section added django updated to django3

--- sf/views.py
+++ sf/views.py
@@ -60,14 +60,16 @@ def release(request, slug):
         tracklist = list(Track.objects.filter(release_id=release_qset).order_by('pos').values(
             'id', 'pos', 'title', 'slug', 'duration', 'track_mp3'))
 
-        release_artitst_ids = ReleaseArtists.objects.filter(release=release_qset).values_list('artist', flat=True)
-        recommended_release_ids = ReleaseArtists.objects.filter(artist__in=release_artitst_ids).exclude(release=release_qset).values_list('release', flat=True)
+        release_artist_ids = ReleaseArtists.objects.filter(release=release_qset).values_list('artist', flat=True)
+        recommended_release_ids = ReleaseArtists.objects.filter(artist__in=release_artist_ids).exclude(release=release_qset).values_list('release', flat=True)
         recommended_releases = Release.objects.filter(id__in=recommended_release_ids)
+        artists = Artist.objects.filter(id__in=release_artist_ids)
 
         return render(request, 'release.html', {'release': release_qset,
                                                 'tracklist': tracklist,
                                                 'tags': tags,
                                                 'recommended_releases': recommended_releases,
+                                                'artists': artists,
                                                 'version': __version__})
 
 
