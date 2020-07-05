24a8f8312dbf99472863c62dab1b9a2b7c21ce49
Max <maxgruzin@gmail.com>
2020-07-05 12:12:24 +0300

    lossless download added

--- sf/models.py
+++ sf/models.py
@@ -49,6 +49,7 @@ class Release(models.Model):
     cover_image = models.ImageField(upload_to='cover_image', null=True, blank=True)
     website_image = models.ImageField(upload_to='website_image', null=True, blank=True)
     download_link = models.TextField(null=True, blank=True)
+    download_link_lossless = models.TextField(null=True, blank=True)
     mentions = models.TextField(null=True, blank=True)
 
     class Meta:
